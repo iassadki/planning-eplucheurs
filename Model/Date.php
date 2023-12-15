4ec7d642b60d609feaf6c94615b8510698f4f98e
Ilias ASSADKI <ilias.assadki@gmail.com>
2023-12-15 12:19:16 +0100

    affichage du tableau en 4 colonnes

--- Model/Date.php
+++ Model/Date.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+
 
 class Date
 {
@@ -9,37 +11,50 @@ class Date
     function getAll($year)
     {
         $weeks = [];
-         
+
         for ($i = 0; $i < 52; $i++) {
-            $week_start = date('d-m-Y', strtotime($year . 'W' . str_pad($i+1, 2, 0, STR_PAD_LEFT)));
+            $week_start = date('d-m-Y', strtotime($year . 'W' . str_pad($i + 1, 2, 0, STR_PAD_LEFT)));
             $week_end = date('d-m-Y', strtotime($week_start . ' +6 days'));
             $weeks[$i] = $week_end;
-            
-        } 
-        ?> 
-            <form action="" method="post">
-            <?php foreach ($weeks as $week) { ?>
-                  <label for="week"><?php echo $week; ?></label>  
-                  <select name="week">
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                </select>   
-            <?php } ?>                   
-                
-                <input type="submit" name="submit" value="Submit">
-            </form>
+        }
+
+        $columns = 4;
+        $weeksCount = count($weeks);
+        $rows = ceil($weeksCount / $columns);
+
+        ?>
+        <form action="" method="post">
+            <table>
+                <?php for ($row = 0; $row < $rows; $row++) { ?>
+                    <tbody>
+                        <tr>
+                            <?php for ($col = 0; $col < $columns; $col++) { ?>
+                                <?php $index = $row * $columns + $col; ?>
+                                <?php if ($index < $weeksCount) { ?>
+                                    <td>
+                                        <label for="week">
+                                            <?php echo $weeks[$index]; ?>
+                                        </label>
+                                    </td>
+                                    <td>
+                                        <select name="week_<?php echo $index; ?>">
+                                            <option value="1">Jordy</option>
+                                            <option value="2">Ilias</option>
+                                            <option value="2">Tumay</option>
+                                        </select>
+                                    </td>
+                                <?php } else { ?>
+                                    <td></td>
+                                    <td></td>
+                                <?php } ?>
+                            <?php } ?>
+                        </tr>
+                    <?php } ?>
+            </table>
+
+            <input type="submit" name="submit" value="Submit">
+        </form>
         <?php
-         
-        
-
-        // for ($week = 1; $week <= 52; $week++) {
-        //     $week_start = date('d-m-Y', strtotime($year . 'W' . str_pad($week, 2, 0, STR_PAD_LEFT)));
-        //     $week_end = date('d-m-Y', strtotime($week_start . ' +6 days'));
-
-        //     $month = date('m', strtotime($week_start));
-
-        //     echo $week_end . ' ';
-        // }
     }
 }
 ?>
