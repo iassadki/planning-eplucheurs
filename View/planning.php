cffa9e258e2bef4f9e9d6a154b8f11fec9642e2e
Jordy <jper220803@gmail.com>
2024-01-07 22:47:28 +0100

    tt

--- View/planning.php
+++ View/planning.php
@@ -1,11 +1,10 @@
-<select name="year" id="year">
-<form action="" method="get">
+<form action="index.php?ctrl=User&action=choseYear" method="get">
     <select name="year" id="year">
         <?php
-        for ($i = 2014; $i <= 2020; $i++) {
-            $selected = ($i == $year) ? 'selected' : '';
-            echo '<option value="' . $i . '" ' . $selected . '>' . $i . '</option>';
-        }
+            for ($i = 2014; $i <= 2020; $i++) {
+                $selected = ($i == $year) ? 'selected' : '';
+                echo '<option value="' . $i . '" ' . $selected . '>' . $i . '</option>';
+            }
         ?>
     </select>
     <input type="submit" value="Show">
