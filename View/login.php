153ef00972bc7ecef50228c84508654189f7406b
Ilias ASSADKI <ilias.assadki@gmail.com>
2024-01-07 22:15:03 +0100

    p

--- View/login.php
+++ View/login.php
@@ -1,7 +1,12 @@
-<form action="index.php?ctrl=User&action=doLogin" method="post">
-    <input type="email" name="email"placeholder="Mail" required/><br>
-    <input type="password" name="password"placeholder="Password" required/><br>
-    <p>
-        <input type="submit" class="submit-btn" value="Connect">
-    </p>
-</form>
+<section id="main-section">
+    <div class="wrapper-50 margin-auto center">
+    <h2>Login</h2>
+    <form class="form" action="index.php?ctrl=User&action=doLogin" method="post">
+        <input type="email" name="email"placeholder="Mail" required/><br>
+        <input type="password" name="password"placeholder="Password" required/><br>
+        <p>
+            <input type="submit" class="submit-btn" value="Connect">
+        </p>
+    </form>
+    <p></p>
+</div></section>
