fa6785a7df2d1b39ec153a7967a5dacd1a8e9913
Ilias ASSADKI <ilias.assadki@gmail.com>
2024-01-07 01:36:11 +0100

    comm

--- Model/test.php
+++ Model/test.php
@@ -7,7 +7,7 @@ $uri = 'mongodb+srv://test:test@cluster0.63c2egn.mongodb.net/?retryWrites=true&w
 // Create a new client and connect to the server
 $client = new MongoDB\Client($uri);
 try {
-    // Send a ping to confirm a successful connection
+    // Send a ping to confirm a successful connection  
     $client->selectDatabase('Planning')->command(['ping' => 1]);
     echo "Pinged your deployment. You successfully connected to MongoDB!\n";
 } catch (Exception $e) {
