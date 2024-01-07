76ad739617c93144c43fb7f8fc1765d4f13bba4b
Ilias ASSADKI <ilias.assadki@gmail.com>
2024-01-07 22:30:59 +0100

    m

--- View/header.php
+++ View/header.php
@@ -16,7 +16,6 @@ $userManager = new UserManager($db);
     </div>
 
     <div id="account_bar">
-        <?php if(isset($_SESSION['user'])) { ?>
         <div class="connection center">
         	<div class="text">Bonjour <?php echo $_SESSION['user']['firstName']; ?></div>
             <a href="./index.php?ctrl=UserController&action=doLogout" class="no-deco" title="Logout account">
@@ -24,18 +23,16 @@ $userManager = new UserManager($db);
                 <div class="text">Logout</div>
             </a>
         </div>
-        <?php } else { ?>
         <div class="connection center">
             <a href="./index.php?ctrl=UserController&action=login" class="no-deco" title="Login or create account">
                 <i class="fas fa-user"></i>
                 <div class="text">Login</div>
             </a>
         </div>
-        <?php } ?>
     </div>
 
     <ul id="menu_bar">
-        <a href="./index.php?ctrl=UserController&action=usersList" class="no-deco"><li>Liste des utilisateurs</li></a>
+        <!-- <a href="./index.php?ctrl=UserController&action=usersList" class="no-deco"><li>Liste des utilisateurs</li></a> -->
         <a href="./index.php?ctrl=UserController&action=usersList" class="no-deco"><li>Votre planning</li></a>
         <!-- <a href="./" class="no-deco"><li>La soutenance</li></a> -->
         <!-- <a href="./" class="no-deco"><li>Le carnet de liaison</li></a> -->
